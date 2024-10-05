2e947f84299e4d819180109dc0da23f1fe90a079
Abdullo Navruzov <149788936+AsaloyBekturdieva@users.noreply.github.com>
2024-10-05 21:54:04 +0500

    merge try

--- src/main/java/com/example/YouTubeApplication.java
+++ src/main/java/com/example/YouTubeApplication.java
@@ -8,10 +8,19 @@ public class YouTubeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(YouTubeApplication.class, args);
+
+		System.out.println("dfghgwdef");
+		System.out.println("dfghgwdef");
+		System.out.println("dfghgwdef");
+		System.out.println("dfghgwdef");
+		System.out.println("dfghgwdef");
+		System.out.println("dfghgwdef");
+		System.out.println("dfghgwdef");
 		System.out.println("qwert");
 		System.out.println("qwert");
 		System.out.println("qwert");
 		System.out.println("hi gitler");
+		System.out.println("werty");
 	}
  // opkopdssccscd
 
